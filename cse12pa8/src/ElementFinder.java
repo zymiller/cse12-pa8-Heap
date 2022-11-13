4e56d8b052c7518c640e651ee6946c1443645e7d
Akshat Singhal <31578650+aksinghal5590@users.noreply.github.com>
2022-11-12 21:33:07 -0800

    Update parameter types

--- cse12pa8/src/ElementFinder.java
+++ cse12pa8/src/ElementFinder.java
@@ -1,6 +1,6 @@
 public class ElementFinder {
 
-	public static int Kth_finder(filename, K, operation) {
+	public static int Kth_finder(String filename, int K, String operation) {
 		/** TODO **/
 		return null;
 	}
