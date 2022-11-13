9235c9c88ed2a69ecebc2695869ea4ff65bf9247
Akshat Singhal <a2singha@ucsd.edu>
2022-11-13 01:42:15 -0800

    Added 1 test as mentioned in the write-up

--- cse12pa8/src/ElementFinder.java
+++ cse12pa8/src/ElementFinder.java
@@ -2,7 +2,7 @@ public class ElementFinder {
 
 	public static int Kth_finder(String filename, int K, String operation) {
 		/** TODO **/
-		return null;
+		return 0;
 	}
 	
 	/* Add any helper methods you find useful */
--- cse12pa8/src/HeapTest.java
+++ cse12pa8/src/HeapTest.java
@@ -1,6 +1,35 @@
 // import static org.junit.*;
 
-import java.util.*;
+import static org.junit.Assert.assertEquals;
 
+import java.util.Comparator;
+
+import org.junit.Test;
+
+/**
+ * HeapTest class implements tester that will test the methods from heap file
+ */
 public class HeapTest {
+
+	@Test
+	public void testAdd() {
+		Comparator<Integer> maxHeapComparator = new Comparator<Integer>() {
+
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o1 - o2;
+			}
+		};
+		Heap<Integer, String> heap = new Heap<Integer, String>(maxHeapComparator);
+		heap.add(19, "");
+		heap.add(50, "10");
+		heap.add(30, "10");
+		heap.add(15, "10");
+		heap.add(20, "10");
+		heap.add(10, "10");
+		heap.add(5, "");
+		heap.add(2, "");
+		assertEquals(8, heap.entries.size());
+	}
+
 }
