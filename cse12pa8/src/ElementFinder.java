6674872061e4c2611c925ceb7ed05ac73c4d3cdb
Akshat Singhal <31578650+aksinghal5590@users.noreply.github.com>
2022-11-13 16:16:02 -0800

    Update ElementFinder.java

--- cse12pa8/src/ElementFinder.java
+++ cse12pa8/src/ElementFinder.java
@@ -2,7 +2,7 @@ public class ElementFinder {
 
 	public static int Kth_finder(String filename, int K, String operation) {
 		/** TODO **/
-		return 0;
+		return -1;
 	}
 	
 	/* Add any helper methods you find useful */
