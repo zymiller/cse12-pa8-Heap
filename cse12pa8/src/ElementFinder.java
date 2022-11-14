c5eb699a4deed2184dd208c5d2621ebe13b91540
Akshat Singhal <31578650+aksinghal5590@users.noreply.github.com>
2022-11-13 16:41:04 -0800

    Update ElementFinder.java

--- cse12pa8/src/ElementFinder.java
+++ cse12pa8/src/ElementFinder.java
@@ -1,6 +1,9 @@
 public class ElementFinder {
 
 	public static int Kth_finder(String filename, int K, String operation) {
+		
+		// Create a comparator depending upon the type of operation
+		// Heap<Integer, Integer> heap = new Heap<Integer, Integer>(comparator);
 		/** TODO **/
 		return -1;
 	}
